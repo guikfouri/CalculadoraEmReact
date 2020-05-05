6b156305ed9faa8a05ff8bd498fa11c822ca33af
guikfouri <guiokfouri@gmail.com>
2020-05-05 10:38:45 -0300

    Updates

---
+++ precache-manifest.d6d2dae9c1ef233ea6bedbec42f3f20e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2204c861712e97ac0ceca4eb4fb7c383",
+    "url": "/calculadora/index.html"
+  },
+  {
+    "revision": "d03673bfd5a03f041e44",
+    "url": "/calculadora/static/css/main.904943da.chunk.css"
+  },
+  {
+    "revision": "6de89b12ddd7ece4fbb9",
+    "url": "/calculadora/static/js/2.a6ec2359.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora/static/js/2.a6ec2359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03673bfd5a03f041e44",
+    "url": "/calculadora/static/js/main.2c00e9cb.chunk.js"
+  },
+  {
+    "revision": "c1b720c9c3a95472f373",
+    "url": "/calculadora/static/js/runtime-main.1a558344.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/calculadora/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
